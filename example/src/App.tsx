0f366d903eb0dd3573f29556b1e6e893817d7f18
Aleksey Pekhterev <qudaeo@yandex.ru>
2024-05-13 16:49:03 +0700

    change to esnext

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -16,12 +16,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
   },
 })
